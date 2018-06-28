5ebfebcb4455f5dd81261970c735560faa9a7845
Paul Senior <paul.senior@cqc.org.uk>
2018-06-28 16:10:36 +0100

    Add project files.

---
+++ HelloAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
